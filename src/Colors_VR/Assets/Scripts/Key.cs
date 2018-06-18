04b7bc01807145f1fddffcda7ccf964915cc0ec9
markus_go <markus.gottschalk@student.htw-berlin.de>
2018-06-18 17:55:53 +0200

    changed ColorPuzzle, added sound for platformAnimation (not implemented yet)

--- src/Colors_VR/Assets/Scripts/Key.cs
+++ src/Colors_VR/Assets/Scripts/Key.cs
@@ -6,14 +6,20 @@ public class Key : MonoBehaviour {
 
     public char character;
     private Keypad keypad;
+    private MeshRenderer meshRenderer;
 
 	void Start ()
     {
         keypad = this.transform.parent.parent.gameObject.GetComponent<Keypad>();
+        meshRenderer = GetComponent<MeshRenderer>();
 	}
 
-    protected virtual void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.GetComponent<PhysicsOrb>() == null)
+            return;
+            
         keypad.SendCharacter(character);
+        meshRenderer.material.color = collision.gameObject.GetComponent<MeshRenderer>().material.color;
     }
 }
