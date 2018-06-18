17f1685af868b03f3379aeefb39b3dcaafb7aaf7
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2018-06-18 12:36:30 +0200

    Add keypad prefab

---
+++ src/Colors_VR/Assets/Scripts/Key.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public char character;
+    private Keypad keypad;
+
+	void Start ()
+    {
+        keypad = this.transform.parent.parent.gameObject.GetComponent<Keypad>();
+	}
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        keypad.SendCharacter(character);
+    }
+}
