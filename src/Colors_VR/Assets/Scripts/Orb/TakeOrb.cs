da313e69758aeb848626cd548bb137b411321e69
Florian Hermuth <s0544767@htw-berlin.de>
2018-07-04 10:55:34 +0200

    Rename bug 2.

---
+++ src/Colors_VR/Assets/Scripts/Orb/TakeOrb.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class TakeOrb : MonoBehaviour {
+
+    public OrbType orbType;
+	public Material viveTrackpadMaterial;
+	
+	public AudioClip takeSound;
+
+	[HideInInspector]
+	public bool taken = false;
+
+	private MeshRenderer meshRenderer;
+
+	private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+		Debug.Log("OnTriggerEnter: " + other.gameObject.layer);
+        if(other.gameObject.layer == 9 && !taken)
+        {
+            taken = true;
+
+			OrbGun[] orbGuns = other.transform.root.GetComponentsInChildren<OrbGun>();
+
+			foreach (OrbGun orbGun in orbGuns)
+				orbGun.SetOrbActive(orbType, viveTrackpadMaterial);
+
+			VRPlayerController vrPlayerController = other.transform.root.GetComponent<VRPlayerController>();
+
+			if (vrPlayerController != null)
+				vrPlayerController.Vibration(ViveController.BOTH, 1.0f);
+
+			AudioSource.PlayClipAtPoint(takeSound, transform.position, 0.5f);
+
+            meshRenderer.enabled = false;
+        }
+    }
+
+	public void SetActive(bool value)
+	{
+		gameObject.SetActive(value);
+	}
+}
